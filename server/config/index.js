dc31d53b77d31ee63f74db9c709571dd52e13321
Joshua Gregory <yoshig5114@gmail.com>
2023-06-15 15:23:47 -0400

    created config for mySQL in .env

---
+++ server/config/index.js
@@ -0,0 +1,24 @@
+// this functions as our file to log into mySQL
+// by means of a file called ".env"
+
+// .gitIgnore will by default not push any files with ".env"
+// in  front of them to gitHub
+// the .env file should be specific to anyone working on the same codebase
+
+const dotenv = require('dotenv');
+
+const envFound = dotenv.config();
+
+if (!envFound) {
+    throw new Error("Couldn't find .env file");
+}
+
+export default {
+    mysql: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_SCHEMA,
+    },
+    port: parseInt(process.env.PORT),
+}
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();//this is an instance of express method
+const PORT = 3001;
 
 // middleware
 app.use(express.json());
@@ -10,6 +11,22 @@ app.get("/", (request, response) => {
     response.end();
 });
 
-app.listen(3001, () => {
-    console.log("Server is running on port 3001");
+app.post("/heroes", (req, res) => {
+    const responseBody = {
+        body: req.body,
+        url: req.url,
+        method: req.method,
+    }
+    res.json(responseBody);
+    res.end();
+})
+
+app.get("/heroes", (req, res) => {
+    res.write("<H2> Batman </H2>");
+    res.status(200);
+    res.end();
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
